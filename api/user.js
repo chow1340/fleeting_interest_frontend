b87c14f501593496b492b33de4a45ea7851caa97
Jeffrey <chow1340@mylaurier.ca>
2020-10-19 15:32:32 -0400

    reset this

--- api/user.js
+++ api/user.js
@@ -4,17 +4,12 @@ import {resolve} from "./resolve";
 import {SET_CURRENT_PROFILE} from '../redux/actionTypes/profileTypes'
 import {useSelector} from "react-redux";
 
-export async function getCurrentUser(dispatch) {
-
-    return await resolve(
-        axios.get(global.server + '/api/user/getCurrentUser')
-        .then(res => {
-          dispatch({type: SET_CURRENT_PROFILE, payload: res.data});
-          console.log(res.data, "ran export");
-        })
-        .catch(err => {
-          console.log(err)
-        })
-        .then(res => res.data)
-    )
+export function getCurrentUser() {
+    axios.get(global.server + '/api/user/getCurrentUser')
+    .then(res => {
+        return res.data;
+    })
+    .catch(err => {
+        console.log(err)
+    })
   }
